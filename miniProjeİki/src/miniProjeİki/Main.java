47a96c405432f2eea4bc2d470512bd8aedd7ee90
creedenceclearwater <creedenceclearwater@LAPTOP-BIT7KUH6>
2022-09-21 08:31:42 +0300

    my second commit

---
+++ miniProjeİki/src/miniProjeİki/Main.java
@@ -0,0 +1,37 @@
+package miniProjeİki;
+
+public class  Main {
+
+    public static void main(String[] args) {
+	char letter = 'K';
+
+    switch (letter){
+        case 'A':
+        case 'U':
+        case 'I':
+        case 'O':
+        case 'E':
+            System.out.println("Sesli Kalýn Harf");
+            break;
+        default:
+            System.out.println("Sessiz ince Harf");
+    }
+    }
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	
+
+
